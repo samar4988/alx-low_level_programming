b327629364100e899a5844e204ec73a11585703e
samar4988 <samar.emad1259@gmail.com>
2023-06-16 16:16:56 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Decription: print 0, 1,  9
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putcher(',');
+			putchar(' ');
+		}
+
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
