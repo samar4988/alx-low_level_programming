0d0c192e2dfbdb0156045b7c5d67af5853a4e997
samar4988 <samar.emad1259@gmail.com>
2023-06-16 17:32:04 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
  * Description: print all possible different combinations of two digits.
  *
- * Return; Always 0 (success)
+ * Return: Always 0 (success)
 */
 
 int main(void)
