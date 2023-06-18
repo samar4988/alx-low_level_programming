b9d15774655e829bbf18919a9afba0dbab29724e
samar4988 <samar.emad1259@gmail.com>
2023-06-18 05:46:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
 #include <unistd.h>
-/*
- * main - A C program that prints a line to the standar error
- * Return 1 (success)
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ *	ssize_t write(int fd. const void *buf, size_t count):
+ *
+ * Return: 1 (error).
 */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1. quo. 59);
 	return (1);
 }
