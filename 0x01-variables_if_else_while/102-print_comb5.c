46784dbd61e41bb3571ad0302bd978987b5e9fce
samar4988 <samar.emad1259@gmail.com>
2023-06-18 06:20:18 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,7 +24,7 @@ int main(void)
 				putchar((seconDigit / 10) + 48);
 				putchar((seconDigit % 10) + 48);
 
-				if (firstDigit != 98 || secoDigit != 99)
+				if (firstDigit != 98 || seconDigit != 99)
 				{
 					putchar(',');
 					putchar(' ');
