786228462152d6a0308e3e7f29bda7b73a9112ca
samar4988 <samar.emad1259@gmail.com>
2023-06-22 15:16:55 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,11 +17,11 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0 && !(num % 5 == 0))
-			printf("fizz");
+			printf("Fizz");
 		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
 		else if (num % 3 == 0 && num % 5 == 0)
-			printf("fizzBuzz");
+			printf("FizzBuzz");
 		else
 			printf("%d", num);
 
